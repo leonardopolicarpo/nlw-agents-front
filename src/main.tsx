42425cd35d6388a5405736d35c7a800c5374c326
leonardo-policarpo <leo.dumont@outlook.com>
2025-07-10 18:27:51 -0300

    feat: configure biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './app'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './app';
 
+// biome-ignore lint/style/noNonNullAssertion: mandatory by reat
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
