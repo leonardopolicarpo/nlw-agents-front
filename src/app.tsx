2813a418710a1cfb127e53384d24bd27855ad506
leonardo-policarpo <leo.dumont@outlook.com>
2025-07-10 19:14:47 -0300

    feat: configure shadcn ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,15 @@
+import { Wand } from 'lucide-react';
+import { Button } from './components/ui/button';
+
 export function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>Teste</Button>
+      <Button variant={'destructive'}>Teste</Button>
+      <br />
+      <Button size={'icon'}>
+        <Wand />
+      </Button>
+    </div>
+  );
 }
