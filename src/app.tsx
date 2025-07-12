4a65e033e79c819448fb493919275abdbad9dc24
leonardo-policarpo <leo.dumont@outlook.com>
2025-07-12 17:06:36 -0300

    feat: implements react-router-dom

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
-import { Wand } from 'lucide-react';
-import { Button } from './components/ui/button';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/create-room';
+import { Room } from './pages/room';
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <div>
-      <Button>Teste</Button>
-      <Button variant={'destructive'}>Teste</Button>
-      <br />
-      <Button size={'icon'}>
-        <Wand />
-      </Button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
